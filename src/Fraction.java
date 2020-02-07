453ed500c0161b6a9fa636ee634b03d85991153d
diaye <yerim-oumar.dia@ssfs.org>
2020-02-07 09:08:13 -0500

    update n fraction class

--- src/Fraction.java
+++ src/Fraction.java
@@ -11,6 +11,39 @@ public class Fraction {
         this.denominator = denominator;
     }
 
+    public static Fraction add(Fraction f1, Fraction f2){
+        int product1 = f1.numerator * f2.denominator;
+        int product2 = f1.denominator * f2.numerator;
+
+        int numeratorSum = product1 + product2;
+        int finalDenominator = f1.denominator * f2.denominator;
+        return new Fraction(numeratorSum, finalDenominator);
+
+    }
+
+    public static Fraction subtract(Fraction f1, Fraction f2){
+        int product1 = f1.numerator * f2.denominator;
+        int product2 = f1.denominator * f2.numerator;
+
+        int finalNumerator = product1 - product2;
+        int finalDenominator = f1.denominator * f2.denominator;
+
+        return new Fraction(finalNumerator, finalDenominator);
+
+    }
+
+    public static Fraction multiply(Fraction f1, Fraction f2){
+        int product1 = f1.numerator * f2.numerator;
+        int product2 = f1.denominator * f2.denominator;
+
+        return new Fraction(product1, product2);
+
+    }
+
+
+
 
 
 }
+
+
