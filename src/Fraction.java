9ecf18e8a667c593f7c8b56c0e8eb7e6ec56d49e
diaye <yerim-oumar.dia@ssfs.org>
2020-02-11 11:35:07 -0500

    DOne with Fraction Class. Final Commit

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,11 +1,10 @@
 /**
  *
- * This Fraction class is able to divide, add, subtract, and simplify one or two fractions
+ * This Fraction class is able to divide, add, subtract, and simplify fractions
  *
  * @author Yerim Dia
  * @since February 10, 2020
  */
-
 
 
 public class Fraction {
@@ -14,29 +13,35 @@ public class Fraction {
 
     /**
      *
-     * @param numerator
-     * @param denominator
+     * @param numerator the variable that wil represent the numerator of the fractions
+     * @param denominator the variable that will represent the denominator of the fractions
      */
-
 
     public Fraction(int numerator, int denominator){
         this.numerator = numerator;
         this.denominator = denominator;
 
-        if (denominator < 0 || numerator < 0){
-            this.numerator = numerator;
-            this.denominator = denominator;
+        if (denominator < 0){
+            this.numerator = numerator *-1 ;
+            this.denominator = denominator * -1;
         }
+        else if (denominator < 0 && numerator < 0){
+            this.numerator = numerator * -1;
+            this.denominator = denominator *-1;
 
-        int gCD = gcd(this.numerator, this.denominator);
+        }
+        int gCD = Math.abs(gcd(this.numerator, this.denominator));
 
         this.numerator/=gCD;
         this.denominator/=gCD;
-
-
     }
 
-
+    /**
+     *
+     * @param m the numerator the fraction
+     * @param n the greates common denominator
+     * @return the greatest common denominator between the two fractions
+     */
     private int gcd(int m, int n) {
         while (m % n != 0) {
             int oldm = m;
@@ -48,13 +53,20 @@ public class Fraction {
         return n;
     }
 
-
-
+    /**
+     * calls the other constructor and assigns default value of 1
+     */
     public Fraction(){
         this(1,1);
     }
 
 
+    /**
+     * This method add two fractions together.
+     * @param f1 the product of the numerator of fraction one multiplied by the denominator of the second
+     * @param f2 the product of the numerator of fraction two multiplied by the denominator of the first
+     * @return it returns a one Fraction with the sum of f1 and f2 over the product of the denominators.
+     */
     public static Fraction add(Fraction f1, Fraction f2){
         int product1 = f1.numerator * f2.denominator;
         int product2 = f1.denominator * f2.numerator;
@@ -65,6 +77,12 @@ public class Fraction {
 
     }
 
+    /**
+     * This method subtracts two fractions.
+     * @param f1  the product of the numerator of fraction one multiplied by the denominator of the second.
+     * @param f2 the product of the numerator of fraction two multiplied by the denominator of the first.
+     * @return it returns a one Fraction with f1 - f2 over the product of the denominators.
+     */
     public static Fraction subtract(Fraction f1, Fraction f2){
         int product1 = f1.numerator * f2.denominator;
         int product2 = f1.denominator * f2.numerator;
@@ -76,6 +94,12 @@ public class Fraction {
 
     }
 
+    /**
+     * This method multiples two fractions. It is done by multiplying the numerators together and the denominators together.
+     * @param f1 the numerator of the first fraction multiplied by the second.
+     * @param f2 the product of the denominators.
+     * @return returns a Fraction with the product of the numerator over the product of the denominators.
+     */
     public static Fraction multiply(Fraction f1, Fraction f2){
         int product1 = f1.numerator * f2.numerator;
         int product2 = f1.denominator * f2.denominator;
@@ -84,6 +108,13 @@ public class Fraction {
 
     }
 
+
+    /**
+     * This method divides two fractions. Using the formula of multiplying the first fraction by the reciprocal of the second
+     * @param f1 the numerator of the first fraction multiplied by the denominator of the second.
+     * @param f2  the numerator of the second fraction multiplied by the denominator of the first.
+     * @return returns a Fraction with the total of product1 over product2.
+     */
     public static Fraction divide(Fraction f1, Fraction f2){
         int product1 = f1.numerator * f2.denominator;
         int product2 = f1.denominator * f2.numerator;
@@ -91,6 +122,10 @@ public class Fraction {
         return new Fraction(product1, product2);
     }
 
+    /**
+     * This method returns the layout of a fraction, numerator/ denominator, ex. 1/2
+     * @return returns the fraction in form a/b.
+     */
     public String toString(){
         return numerator + "/" + denominator;
     }
